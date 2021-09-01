406d8e4002d4e4ddb57c864c27ec71ea74120ddd
pohjie <pohjie.pj@gmail.com>
2021-09-01 18:47:34 +0800

    implement work timer

--- App.js
+++ App.js
@@ -2,15 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +9,43 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text_block: {
+    alignItems: 'center',
+    fontSize: 48,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+  },
 });
+
+class CountdownTimer extends React.Component {
+  state = {
+    seconds: 1500,
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.decrementSec, 1000)
+  }
+
+  decrementSec = () => {
+    this.setState(prevState => ({seconds: prevState.seconds - 1}))
+  }
+
+  render() {
+    return <Text style={styles.text_block}>
+              {~~(this.state.seconds / 60)}:{this.state.seconds % 60}
+          </Text>
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text_block}>Work Timer</Text>
+        <CountdownTimer />
+      <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
